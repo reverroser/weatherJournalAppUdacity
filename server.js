8bc6bbf1e10baf756f722bb5e7409b5c4fc1c566
Roser Reverte <reverroser@gmail.com>
2020-03-17 17:36:38 +0100

    Node and Express Environment setup

---
+++ server.js
@@ -0,0 +1,26 @@
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
+
+/* Middleware */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+const port = 8000;
+// Callback function to debug
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+};
+//Spin up the server
+const server = app.listen(port, listening);
